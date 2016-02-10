6bdd0a41c1ae99cab4730f16695bda32200131f8
Adam Trabold <adam@adamtrabold.com>
2016-02-09 17:51:02 -0800

    • Fixed bug that happened when user pushed an operator more than once • Set default to 0

--- retro-calculator/ViewController.swift
+++ retro-calculator/ViewController.swift
@@ -90,21 +90,25 @@ class ViewController: UIViewController {
         
         if currentOperation != Operation.Empty {
             //Run some math
-            rightValStr = runningNumber
-            runningNumber = ""
             
-            if currentOperation == Operation.Multiply {
-                result = "\(Double(leftValStr)! * Double(rightValStr)!)"
-            } else if currentOperation == Operation.Divide {
-                result = "\(Double(leftValStr)! / Double(rightValStr)!)"
-            } else if currentOperation == Operation.Subtract {
-                result = "\(Double(leftValStr)! - Double(rightValStr)!)"
-            } else if currentOperation == Operation.Add {
-                result = "\(Double(leftValStr)! + Double(rightValStr)!)"
-            }
+            //A user selected an operator, but then selected another operator without first entering a number
+            if runningNumber != "" {
+                rightValStr = runningNumber
+                runningNumber = ""
+            
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                }
             
             leftValStr = result
             outputLbl.text = result
+            }
             
             currentOperation = op
             
